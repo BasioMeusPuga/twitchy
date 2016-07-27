d5439578f3655495e078134226c4a41c2ec654a8
BasioMeusPuga <disgruntled.mob@gmail.com>
2016-07-27 20:51:55 +0530

    Deleted a variable I didn't use so look out world energy shortage.

--- twitchy.sh
+++ twitchy.sh
@@ -4,8 +4,8 @@
 #Options
 database="$HOME/.twitchy.db"
 video_player=mpv
-mpv_hardware_acceleration=no
-quality=high
+mpv_hardware_acceleration=no 		#Uses vaapi if enabled
+quality=high 		#Valid options are: low / medium / high / source
 truncate_status_at=100
 number_of_faves=10
 show_offline=no
@@ -14,7 +14,7 @@ show_offline=no
 notify_sound=""
 check_interval_seconds=60
 
-# Put yo' spicy memes here
+#Put yo' spicy memes here
 memes_everywhere=no
 meme="RAISE YOUR DONGERS
  E S P O R T S
@@ -137,14 +137,19 @@ else
 fi
 
 if [[ "$day" = 0 ]]; then
-	time_watched_hms=$(echo "$(printf "%02d" $hour)"h "$(printf "%02d" $min)"m "$(printf "%02d" $sec)"s)
+	time_watched_hms="$(printf "%02d" $hour)h $(printf "%02d" $min)m $(printf "%02d" $sec)s"
 else
-	time_watched_hms=$(echo "$(printf "%02d" $day)"d "$(printf "%02d" $hour)"h "$(printf "%02d" $min)"m "$(printf "%02d" $sec)"s)
+	time_watched_hms="$(printf "%02d" $day)d $(printf "%02d" $hour)h $(printf "%02d" $min)m $(printf "%02d" $sec)s"
 fi
 }
 
 #Sanity checks
- if [[ ! -f /usr/bin/toilet ]]; then
+if [[ ! -f /usr/bin/livestreamer ]]; then
+	echo " livestreamer not installed. FeelsBadMan."
+	exit
+fi
+
+if [[ ! -f /usr/bin/toilet ]]; then
 	memes_everywhere=no
 fi
 
@@ -155,9 +160,10 @@ fi
 
 if [[ ! -f "$database" ]]; then
 	if [[ $1 != "-w" ]]; then
-		echo " First run. Creating db and exiting."
-		sqlite3 $database "create table channels (id INTEGER PRIMARY KEY,Name TEXT,TimeWatched INTEGER, AltName TEXT);"
-		sqlite3 $database "create table games (id INTEGER PRIMARY KEY,Name TEXT,AltName TEXT);"
+		echo -ne " First run. Creating db and exiting.
+ PLEASE GO THROUGH THE FIRST FEW LINES OF THE SCRIPT TO SET YOUR PREFERRED PLAYER, QUALITY AND DISPLAY SETTINGS\n"
+		sqlite3 "$database" "create table channels (id INTEGER PRIMARY KEY,Name TEXT,TimeWatched INTEGER, AltName TEXT);"
+		sqlite3 "$database" "create table games (id INTEGER PRIMARY KEY,Name TEXT,AltName TEXT);"
 		exit
 	else
 		how_can_tables_be_real_if_our_databases_arent_real=1
@@ -173,7 +179,7 @@ fi
 #Check status of each stream that meets criteria
 function get_status {
 if [[ $fav_mode = 1 ]]; then
-	stream[$1]=$(curl -s https://api.twitch.tv/kraken/streams/$(echo $line | cut -d "|" -f2))
+	stream[$1]=$(curl -s https://api.twitch.tv/kraken/streams/"$(echo $line | cut -d "|" -f2)")
 	fav_time[$1]=$(echo $line | cut -d "|" -f1)
 else
 	stream[$1]=$(curl -s https://api.twitch.tv/kraken/streams/$line)
@@ -191,7 +197,7 @@ if [[ $status = "" ]]; then
 		echo ${fav_time[$line]}"^"$(echo $line | cut -d "|" -f2)";"$game_name";"$channel_status";"$channel_viewers >> /tmp/twitchyfinal
 	else
 	if [[ $monitor_mode = 1 ]]; then
-		echo $1 >> /tmp/twitchynowonline
+		echo "$1" >> /tmp/twitchynowonline
 	else
 		echo $line";"$game_name";"$channel_status";"$channel_viewers";"$display_name";"$partner_status >> /tmp/twitchyfinal
 	fi
@@ -212,13 +218,13 @@ trap ctrl_c EXIT
 function ctrl_c() {
 if [[ $run_once != "yes" ]]; then
 	end_time=$(date +%s)
-	time_watched=$[ $end_time - $start_time ]
+	time_watched=$(( end_time - start_time ))
 	if [[ $final_selection != "" ]] && [[ $start_time != 0 ]]; then
-		time_old=$(sqlite3 $database "select TimeWatched from channels where Name = '$final_selection';")
-		time_new=$[ $time_old + $time_watched ]
-		sqlite3 $database "update channels set TimeWatched = '$time_new' where Name = '$final_selection';"
-		total_time_spent=$(sqlite3 $database "select TimeWatched from channels where Name = '$final_selection';")
-		time_rank=$(sqlite3 $database "select TimeWatched,Name from channels where TimeWatched > 0;" | sort -gr | cat -n | grep $final_selection | awk '{print $1}')
+		time_old=$(sqlite3 "$database" "select TimeWatched from channels where Name = '$final_selection';")
+		time_new=$(( time_old + time_watched ))
+		sqlite3 "$database" "update channels set TimeWatched = '$time_new' where Name = '$final_selection';"
+		total_time_spent=$(sqlite3 "$database" "select TimeWatched from channels where Name = '$final_selection';")
+		time_rank=$(sqlite3 "$database" "select TimeWatched,Name from channels where TimeWatched > 0;" | sort -gr | cat -n | grep "$final_selection" | awk '{print $1}')
 		convert_time $total_time_spent
 		total_time_spent=$time_watched_hms
 		echo -e " Total time spent watching "$final_selection" - "'\E[1;97m'$total_time_spent" ("$time_rank")"'\E[0m'
@@ -261,7 +267,7 @@ exit
 "-a")
 
 channel_name=$2
-curl -s https://api.twitch.tv/kraken/streams/$channel_name > /tmp/twitchy
+curl -s https://api.twitch.tv/kraken/streams/"$channel_name" > /tmp/twitchy
 grep -q \"status\":404 /tmp/twitchy
 if [[ $? = 0 ]]; then
 	echo " $channel_name doesn't exist"
@@ -269,12 +275,12 @@ if [[ $? = 0 ]]; then
 		emote --doot
 	fi
 else
-	if_already_added=$(sqlite3 $database "select Name from channels where Name = '$channel_name';")
+	if_already_added=$(sqlite3 "$database" "select Name from channels where Name = '$channel_name';")
 		if [[ $if_already_added != "" ]]; then
 		echo " $channel_name already in database"
 		exit
 		fi
-	sqlite3 $database "insert into channels (Name,TimeWatched) values ('$channel_name',0);"
+	sqlite3 "$database" "insert into channels (Name,TimeWatched) values ('$channel_name',0);"
 	echo " $channel_name added to database"
 if [[ $memes_everywhere = "yes" ]]; then
 	emote --pogchamp
@@ -288,19 +294,18 @@ fi
 read -p " Replace (s)treamer or (g)ame name: " replace_category
 if [[ $replace_category = "s" ]]; then
 	replace_streamer=1
-	sqlite3 $database "select Name,AltName from channels;" | sort > /tmp/twitchy
+	sqlite3 "$database" "select Name,AltName from channels;" | sort > /tmp/twitchy
 else
 if [[ $replace_category = "g" ]]; then
-	replace_game=1
-	sqlite3 $database "select Name,AltName from games;" | sort > /tmp/twitchy
+	sqlite3 "$database" "select Name,AltName from games;" | sort > /tmp/twitchy
 fi
 fi
 
 i=0
 while read line
 do
-	real_name[i]=$(echo $line | cut -d "|" -f1)
-	alternate_name[i]=$(echo $line | cut -d "|" -f2)
+	real_name[i]=$(echo "$line" | cut -d "|" -f1)
+	alternate_name[i]=$(echo "$line" | cut -d "|" -f2)
 	if [[ ${alternate_name[$i]} = "" ]]; then
 		alternate_name[i]="UNSET"
 	else
@@ -313,25 +318,25 @@ do
 		space=45
 	fi
 
-	a_var=$[ $i +1 ]
+	a_var=$(( i +1 ))
 	echo -ne " "'\E[93m'$a_var'\E[0m'
 	if [[ $a_var -lt 10 ]]; then
 		printf "  ""%-"$space"s %-"$space"s\n" "${real_name[$i]}" "${alternate_name[$i]}"
 		else
 		printf " ""%-"$space"s %-"$space"s\n" "${real_name[$i]}" "${alternate_name[$i]}"
 	fi
-i=$[ $i + 1 ]
+i=$(( i + 1 ))
 done < /tmp/twitchy
 
 	read -p " Select number: " select_number
-	select_number=$[ $select_number -1 ]
+	select_number=$(( select_number -1 ))
 	final_selection=${real_name[$select_number]}
 	read -p " Replace $final_selection with: " final_name
 
 if [[ $replace_streamer = 1 ]]; then
-	sqlite3 $database "update channels set AltName = '$final_name' where Name = '$final_selection';"
+	sqlite3 "$database" "update channels set AltName = '$final_name' where Name = '$final_selection';"
 else
-	sqlite3 $database "update games set AltName = '$final_name' where Name = '$final_selection';"
+	sqlite3 "$database" "update games set AltName = '$final_name' where Name = '$final_selection';"
 fi
 ;;
 
@@ -348,13 +353,13 @@ if [[ $user_id != "" ]]; then
 	fi
 fi
 
-total_followed=$(cat /tmp/twitchy | sed 's/,/\n/g' | grep "_total" | cut -d ":" -f2)
+total_followed=$(sed 's/,/\n/g' /tmp/twitchy | grep "_total" | cut -d ":" -f2)
 curl -s "https://api.twitch.tv/kraken/users/$user_id/follows/channels?limit=$total_followed" > /tmp/twitchy
 
-cat /tmp/twitchy | sed 's/,/\n/g' | grep -v "display" | grep \"name\" | grep -v \"status\" | cut -d ":" -f2- | tr -d "\"" | sort > /tmp/twitchynew
-sqlite3 $database "select Name from channels;" | sort > /tmp/twitchyalreadyfollowed
+sed 's/,/\n/g' /tmp/twitchy | grep \"name\" | grep -v \"status\" | cut -d ":" -f2- | tr -d "\"" | sort > /tmp/twitchynew
+sqlite3 "$database" "select Name from channels;" | sort > /tmp/twitchyalreadyfollowed
 comm -1 -3 /tmp/twitchyalreadyfollowed /tmp/twitchynew > /tmp/twitchyadd
-	new_additions=$(cat /tmp/twitchyadd | wc -l)
+	new_additions=$(wc -l < /tmp/twitchyadd)
 	if [[ $new_additions = 0 ]]; then
 		echo " All streams already in local database. dansgame."
 		exit
@@ -364,7 +369,7 @@ echo -ne " "'\E[93m'"$new_additions channels added to database:"'\E[0m'"\n"
 # 	echo " $new_additions channels added to database:"
 while read line
 do
-	sqlite3 $database "insert into channels (Name,TimeWatched) values ('$line',0);"
+	sqlite3 "$database" "insert into channels (Name,TimeWatched) values ('$line',0);"
 	echo " "$line
 done < /tmp/twitchyadd
 
@@ -378,15 +383,15 @@ fi
 "-n"|"-d")
 
 if [[ $1 = "-n" ]]; then
-	sqlite3 $database "select Name from channels;" > /tmp/twitchy
-	totalstreams=$(cat /tmp/twitchy | wc -l)
+	sqlite3 "$database" "select Name from channels;" > /tmp/twitchy
+	totalstreams=$(wc -l < /tmp/twitchy)
 
 	while read line
 	do
 		get_status $line &
 	done < /tmp/twitchy
 
-	while [[ $(cat /tmp/twitchyfinal | wc -l) != $totalstreams ]]
+	while [[ $(wc -l < /tmp/twitchyfinal) != $totalstreams ]]
 	do
 		sleep .1
 	done 2> /dev/null
@@ -395,29 +400,29 @@ if [[ $1 = "-n" ]]; then
 	mv /tmp/twitchytmp /tmp/twitchyfinal
 	sort /tmp/twitchyfinal -o /tmp/twitchyfinal
 else
-	sqlite3 $database "select Name from channels;" | sort > /tmp/twitchyfinal
+	sqlite3 "$database" "select Name from channels;" | sort > /tmp/twitchyfinal
 fi
 
 i=0
 while read line
 do
 	channel_name[i]="$line"
-	a_var=$[ $i +1 ]
-	echo -e " "'\E[93m'$a_var'\E[0m' $line
-	i=$[ $i + 1 ]
+	a_var=$(( i +1 ))
+	echo -e " "'\E[93m'$a_var'\E[0m' "$line"
+	i=$(( i + 1 ))
 done < /tmp/twitchyfinal
 
 echo -n " Channel number(s): "
 read -a game_number
 number_of_channels=${#game_number[@]}
 
-for check_channels in $(seq 0 $[ $number_of_channels -1 ])
+for check_channels in $(seq 0 $(( number_of_channels -1 )))
 do
-	intermediate_selection=$[ ${game_number[$check_channels]} -1 ]
+	intermediate_selection=$(( ${game_number[$check_channels]} -1 ))
 	final_selection=${channel_name[$intermediate_selection]}
  	
  	if [[ $1 = "-d" ]]; then
- 		sqlite3 $database "delete from channels where Name = '$final_selection';"
+ 		sqlite3 "$database" "delete from channels where Name = '$final_selection';"
 		if [[ $memes_everywhere = "yes" ]]; then
 			echo " "$final_selection" R E K T" | toilet -f smblock --gay
 		else
@@ -446,7 +451,7 @@ while read line
  		get_status $line
 	done < /tmp/twitchynotify
 
-	now_online=$(cat /tmp/twitchynowonline | sed ':a;N;$!ba;s/\n/ | /g')
+	now_online=$(sed ':a;N;$!ba;s/\n/ | /g' /tmp/twitchynowonline)
 	if [[ $now_online != "" ]]; then
 		notify_message=$(echo -e "Now online:\n"$now_online)
 		if [[ $notify_sound != "" ]]; then
@@ -470,12 +475,12 @@ kill $(cat /tmp/twitchypid) &> /dev/null
 if [[ $1 = "-f" ]]; then
 	fav_mode=1
 	show_offline=yes
- 	sqlite3 $database "select TimeWatched,Name from channels where TimeWatched > 0;" | sort -gr | head -n$number_of_faves > /tmp/twitchy
+ 	sqlite3 "$database" "select TimeWatched,Name from channels where TimeWatched > 0;" | sort -gr | head -n$number_of_faves > /tmp/twitchy
 else
 if [[ $1 = "-fr" ]]; then
 	read -p " Reset time watched? (yes/n) " confirm_reset
 	if [[ $confirm_reset = "yes" ]]; then
-		sqlite3 $database "update channels set TimeWatched = 0;"
+		sqlite3 "$database" "update channels set TimeWatched = 0;"
 	fi
 	exit
 else
@@ -488,12 +493,12 @@ if [[ $1 = "-w" ]]; then
 	done
 else
 	channel_arg=$1
-	sqlite3 $database "select Name from channels where Name like '%$channel_arg%' or AltName like '%$channel_arg%';" > /tmp/twitchy
+	sqlite3 "$database" "select Name from channels where Name like '%$channel_arg%' or AltName like '%$channel_arg%';" > /tmp/twitchy
 fi
 fi
 fi
 
-totalstreams=$(cat /tmp/twitchy | wc -l)
+totalstreams=$(wc -l < /tmp/twitchy)
 if [[ $totalstreams = 0 ]]; then
 	if [[ $memes_everywhere = "yes" ]]; then
 		emote --kappa
@@ -522,7 +527,7 @@ do
 	get_status $line &
 done < /tmp/twitchy
 
-while [[ $(cat /tmp/twitchyfinal | wc -l) != $totalstreams ]]
+while [[ $(wc -l < /tmp/twitchyfinal) != $totalstreams ]]
 do
 	sleep .1
 done 2> /dev/null
@@ -537,7 +542,7 @@ fi
 spacex="                      "
 if [[ $fav_mode != 1 ]]; then
 	spacex2="                         "
-	truncate_status_at=$[ $truncate_status_at + 15 ]
+	truncate_status_at=$(( truncate_status_at + 15 ))
 else
 	spacex2="                        "
 fi
@@ -558,7 +563,7 @@ do
 			stream_name=$real_name_stream
 		fi
 	if [[ $how_can_tables_be_real_if_our_databases_arent_real != 1 ]]; then
-		alt_name_stream=$(sqlite3 $database "select AltName from channels where Name like '$stream_name';") 2> /dev/null
+		alt_name_stream=$(sqlite3 "$database" "select AltName from channels where Name like '$stream_name';") 2> /dev/null
 		if [[ $alt_name_stream != "" ]]; then
 			stream_name=$alt_name_stream
 		fi
@@ -572,7 +577,7 @@ do
 		game_played[i]=$game_name
 		
 		if [[ $how_can_tables_be_real_if_our_databases_arent_real != 1 ]]; then
-			alt_name_game=$(sqlite3 $database "select AltName from games where Name = '$game_name';")
+			alt_name_game=$(sqlite3 "$database" "select AltName from games where Name = '$game_name';")
 			if [[ $alt_name_game != "" ]]; then
 				game_name=$alt_name_game
 			fi
@@ -589,22 +594,22 @@ do
 				echo $game_name >> /tmp/twitchygamesshown
 				echo -e " "'\E[96m'$game_name'\E[0m'
 			fi
-			a_var=$[ $i +1 ]
+			a_var=$(( i +1 ))
 			if [[ $a_var -gt 9 ]]; then
 				spacex="                     "
 			fi
 			echo -ne " "'\E[93m'$a_var'\E[0m'
 			printf " "'\E[92m'"%s %s $stream_viewers${spacex2:${#stream_viewers}}$stream_status \E[0m\n" $stream_name"${spacex:${#stream_name}}"
-			i=$[ $i + 1 ]
+			i=$(( i + 1 ))
 		else
-			a_var=$[ $i +1 ]
+			a_var=$(( i +1 ))
 			if [[ $a_var -gt 9 ]]; then
 				spacex="                     "
 			fi
-			game_name=$(echo '\E[96m'$game_name'\E[0m'" - "'\E[92m'$stream_status'\E[0m')
+			game_name='\E[96m'$game_name'\E[0m'" - "'\E[92m'$stream_status'\E[0m'
 			echo -ne " "'\E[93m'$a_var'\E[0m'
 			printf " "'\E[92m'"%s %s $fav_time${spacex2:${#fav_time}}$game_name \E[0m\n" $stream_name"${spacex:${#stream_name}}"
-			i=$[ $i + 1 ]
+			i=$(( i + 1 ))
 		fi
 	else
 		
@@ -644,7 +649,7 @@ if [[ $number_of_channels -gt 1 ]]; then
 	multi_twitch=yes
 fi
 
-for check_channels in $(seq 0 $[ $number_of_channels -1 ])
+for check_channels in $(seq 0 $(( number_of_channels -1 )))
 do
 	if [[ ${game_number[$check_channels]} -gt $a_var ]]; then
 		if [[ $memes_everywhere = "yes" ]]; then
@@ -657,9 +662,9 @@ do
 	fi
 done
 
-for check_channels in $(seq 0 $[ $number_of_channels -1 ])
+for check_channels in $(seq 0 $(( number_of_channels -1 )))
 do
-	game_number[$check_channels]=$[ ${game_number[$check_channels]} -1 ]
+	game_number[$check_channels]=$(( ${game_number[$check_channels]} -1 ))
 	final_selection[$check_channels]=${channel_name[${game_number[$check_channels]}]}
 	quality_check[$check_channels]=$(echo ${for_quality[$check_channels]} | cut -d "-" -f2)
 	
@@ -692,9 +697,9 @@ do
 	fi
 	
 	if [[ $custom_quality = 1 ]]; then
-		now_watching=$(echo $now_watching $(echo ${final_selection[$check_channels]}) "-" ${final_quality[$check_channels]} "|" )
+		now_watching=$now_watching" "${final_selection[$check_channels]}" - "${final_quality[$check_channels]}" |"
 	else
-		now_watching=$(echo $now_watching $(echo ${final_selection[$check_channels]}) "|" )
+		now_watching=$now_watching" "${final_selection[$check_channels]}" |"
 	fi
 done
 
@@ -705,7 +710,7 @@ else
 fi
 echo -e " Now watching: "'\E[1;97m'${now_watching::-1}'\E[0m'
 
-for check_channels in $(seq 0 $[ $number_of_channels -2 ])
+for check_channels in $(seq 0 $(( number_of_channels -2 )))
 do
 	if [[ $video_player = "mpv" ]]; then
 	if [[ $mpv_hardware_acceleration = "yes" ]]; then
@@ -722,9 +727,9 @@ done
 if [[ $multi_twitch != "yes" ]]; then
 	if [[ $onlywatch_mode != 1 ]]; then
 		start_time=$(date +%s)
-		played_before=$(sqlite3 $database "select Name from games where Name = '${game_played[$game_number]}';")
+		played_before=$(sqlite3 "$database" "select Name from games where Name = '${game_played[$game_number]}';")
 		if [[ $played_before = "" ]]; then
-			sqlite3 $database "insert into games (Name) values ('${game_played[$game_number]}');"
+			sqlite3 "$database" "insert into games (Name) values ('${game_played[$game_number]}');"
 		fi
 	fi
 	
@@ -735,7 +740,7 @@ if [[ $multi_twitch != "yes" ]]; then
 	fi
 		
 fi
-	final_channel=$[ $number_of_channels -1 ]
+	final_channel=$(( number_of_channels -1 ))
 	if [[ $video_player = "mpv" ]]; then
 	if [[ $mpv_hardware_acceleration = "yes" ]]; then
 		video_player_final=$video_player" --hwdec=vaapi --vo=vaapi --cache 8192 --title ${final_selection[$final_channel]}"
