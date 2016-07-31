24c333696358036e24805b1e9b12e7b66da0b205
BasioMeusPuga <disgruntled.mob@gmail.com>
2016-07-31 15:01:11 +0530

    Sorting established: Works for one stream as long as there's an existing db

--- twitchy.py
+++ twitchy.py
@@ -21,19 +21,16 @@ class colors:
     OFFLINERED = '\033[93m'
     ENDC = '\033[0m'
     
-stream_status = {}
+stream_status = []
 def get_status(channel_name):
 	r = requests.get('https://api.twitch.tv/kraken/streams/' + channel_name)
 	stream_data = json.loads(r.text)
 
-	if str(stream_data['stream']) == "None":
-		stream_status[channel_name] = [ "Offline" ]
-	else:
-		stream_status[channel_name] = [ stream_data['stream']['channel']['game'], stream_data['stream']['channel']['display_name'], stream_data['stream']['channel']['status'], stream_data['stream']['viewers'], stream_data['stream']['channel']['partner'] ]
+	if str(stream_data['stream']) != "None":
+		stream_status.append([stream_data['stream']['channel']['display_name'], stream_data['stream']['channel']['game'] , stream_data['stream']['channel']['status'], stream_data['stream']['viewers'], stream_data['stream']['channel']['partner']])
 	"""Dictionary Scheme
-	Key: Channel name
-	List0: Game Name
-	List1: Display name
+	List0: Display name
+	List1: Game Name
 	List2: Stream Status
 	List3: Viewers
 	List4: Partner Status"""
@@ -43,24 +40,23 @@ results = pool.map(get_status, names_from_db)
 pool.close()
 pool.join() 
 
-# Sorting code goes here:
+stream_status = sorted(stream_status, key=lambda x: (x[1], -x[3]))
 
 stream_final = []
-template = "{0:20}{1:10}{2:100}"
-
 games_shown = []
 display_number = 1
+template = "{0:20}{1:15}{2:100}"
+
 for i in stream_status:
-	if stream_status[i][0] != "Offline":
-		if stream_status[i][0] not in games_shown:
-			print (colors.GAMECYAN + stream_status[i][0] + colors.ENDC)
-			games_shown.append(stream_status[i][0])
+	if i[1] not in games_shown:
+		print (" " + colors.GAMECYAN + i[1] + colors.ENDC)
+		games_shown.append(i[1])
 	
-		stream_final.insert(display_number - 1, stream_status[i][1].lower())
-		print (colors.NUMBERYELLOW + (str(display_number) + colors.ENDC) + " " + (colors.ONLINEGREEN + template.format(stream_status[i][1], str(stream_status[i][3]), stream_status[i][2]) + colors.ENDC))
-		display_number = display_number + 1
+	stream_final.insert(display_number - 1, i[0].lower())
+	print (" " + colors.NUMBERYELLOW + (str(display_number) + colors.ENDC) + " " + (colors.ONLINEGREEN + template.format(i[0], str(i[3]), i[2]) + colors.ENDC))
+	display_number = display_number + 1
 
 stream_select = int(input("Channel number? "))
 final_selection = stream_final[stream_select - 1]
-print ("Now watching " + final_selection)
+print (" Now watching " + final_selection)
 system('livestreamer twitch.tv/' + final_selection + ' high --player mpv --hls-segment-threads 3')
