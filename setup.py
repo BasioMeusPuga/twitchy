214c8c06d90834feaa390bd70bfc866b8339a566
BasioMeusPuga <disgruntled.mob@gmail.com>
2017-10-28 03:26:44 +0530

    v3.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 MAJOR_VERSION = '3'
-MINOR_VERSION = '0'
+MINOR_VERSION = '1'
 MICRO_VERSION = '0'
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
